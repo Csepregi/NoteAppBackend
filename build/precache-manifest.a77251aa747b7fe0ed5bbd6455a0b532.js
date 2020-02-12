39a350771e2bbadf51d277f63aae07ccba5063b7
Csepregi <csepregigabor02@gmail.com>
2020-02-12 17:43:19 +0100

    uibuild

---
+++ build/precache-manifest.a77251aa747b7fe0ed5bbd6455a0b532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86676c1083dd0c317cf56b568b8f8fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f53aa6b941ca7d8c47",
+    "url": "/static/css/main.0ad4d417.chunk.css"
+  },
+  {
+    "revision": "0daabcda8fb5caa88644",
+    "url": "/static/js/2.f4ac1666.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f4ac1666.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3f53aa6b941ca7d8c47",
+    "url": "/static/js/main.6ffbae69.chunk.js"
+  },
+  {
+    "revision": "e8a34a55854bfa62fff3",
+    "url": "/static/js/runtime-main.32cd00d8.js"
+  }
+]);
